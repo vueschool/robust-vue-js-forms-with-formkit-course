08b41f7e2fda98c8fbbafcb696a5c3f1430dea25
Daniel Kelly <me@danielkelly.io>
2022-11-17 16:19:32 -0600

    3-powerful-form-fields-with-a-simple-api

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <FormKit
+      label="Username"
+      name="username"
+      type="text"
+      help="Pick a new username"
+      validation="required|matches:/^@[a-zA-Z]+$/|length:5"
+      value="@useFormKit"
+      prefix-icon="avatarMan"
+      inner-class="mycustom-inner"
+    >
+      <template #label="context">
+        {{ context.label }}
+        ?
+        <span>{{ context.help }}</span>
+      </template>
+      <template #help></template>
+    </FormKit>
+    <FormKit type="select" />
+    <FormKit type="textarea" />
   </div>
 </template>
