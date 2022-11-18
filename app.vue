ddfc20fa5a33e246da574d6c53f52c55318d27e0
Daniel Kelly <me@danielkelly.io>
2022-11-18 11:40:09 -0600

    lesson 4 start

--- app.vue
+++ app.vue
@@ -1,23 +1,10 @@
+<script setup></script>
 <template>
   <div>
-    <FormKit
-      label="Username"
-      name="username"
-      type="text"
-      help="Pick a new username"
-      validation="required|matches:/^@[a-zA-Z]+$/|length:5"
-      value="@useFormKit"
-      prefix-icon="avatarMan"
-      inner-class="mycustom-inner"
-    >
-      <template #label="context">
-        {{ context.label }}
-        ?
-        <span>{{ context.help }}</span>
-      </template>
-      <template #help></template>
+    <FormKit type="form">
+      <h1>Login</h1>
+      <FormKit type="text" label="Username" name="username" />
+      <FormKit type="password" label="Password" name="password" />
     </FormKit>
-    <FormKit type="select" />
-    <FormKit type="textarea" />
   </div>
 </template>
