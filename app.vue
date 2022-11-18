f37f44f4dc46832422338989249c7cdb81380c3d
Daniel Kelly <me@danielkelly.io>
2022-11-18 13:31:17 -0600

    4-form-population-and-submission

--- app.vue
+++ app.vue
@@ -1,7 +1,16 @@
-<script setup></script>
+<script setup>
+const formData = ref({
+  username: "danielkelly_io",
+  password: "",
+});
+async function handleSubmit(data) {
+  await wait(3000);
+  console.log(data);
+}
+</script>
 <template>
   <div>
-    <FormKit type="form">
+    <FormKit type="form" :value="formData" @submit="handleSubmit">
       <h1>Login</h1>
       <FormKit type="text" label="Username" name="username" />
       <FormKit type="password" label="Password" name="password" />
