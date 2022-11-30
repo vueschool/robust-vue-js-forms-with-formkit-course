ef7b643893d98b55656c538a3b7c2dbf0c040fcf
Daniel Kelly <me@danielkelly.io>
2022-11-30 11:28:34 -0600

    8-create-custom-validation-messages

--- formkit.config.ts
+++ formkit.config.ts
@@ -2,6 +2,15 @@ import { DefaultConfigOptions } from "@formkit/vue";
 
 const config: DefaultConfigOptions = {
   theme: "genesis",
+  messages: {
+    en: {
+      validation: {
+        username_is_unique({ args, name, node }) {
+          return `${node.value} is already taken`;
+        },
+      },
+    },
+  },
   rules: {
     username_is_unique(node) {
       const usernames = ["jpschroeder", "luanguyen", "danielkelly_io"];
