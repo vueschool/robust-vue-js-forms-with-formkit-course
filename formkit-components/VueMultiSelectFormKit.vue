d204ee30977e14ef66e0e24e32121c1770a84592
Daniel Kelly <me@danielkelly.io>
2022-12-02 11:36:28 -0600

    12-the-formkit-custom-input-checklist

--- formkit-components/VueMultiSelectFormKit.vue
+++ formkit-components/VueMultiSelectFormKit.vue
@@ -8,8 +8,18 @@ const props = defineProps({
 <template>
   <MultiSelect
     :model-value="context._value"
-    :options="context.attrs.options"
+    v-bind="context.attrs"
     @update:model-value="context.node.input($event)"
+    :id="context.id"
+    :name="context.node.name"
+    @blur="context.handlers.blur"
+    :disabled="context.disabled"
+    :class="context.classes.input"
   ></MultiSelect>
 </template>
 <style src="vue-multiselect/dist/vue-multiselect.css"></style>
+<style>
+.formkit-input.multiselect {
+  padding: 0;
+}
+</style>
